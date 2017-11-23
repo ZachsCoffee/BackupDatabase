c0a853e8ac920e60fe85361ad5e0b33253efedab
Zachs <georgenascos96@gmail.com>
2017-11-23 23:16:17 +0200

    Update for class, ObjectStream

--- DataBaseBackup/Class/ObjectStream.cs
+++ DataBaseBackup/Class/ObjectStream.cs
@@ -11,6 +11,8 @@ namespace DataBaseBackup.Class
     public class ObjectStream
     {
         private string filePath;
+        private int index = -1, step;
+        private StreamReader stepReader;
 
         public ObjectStream(string filePath)
         {
@@ -36,6 +38,28 @@ namespace DataBaseBackup.Class
             return list;
         }
 
+        public void EditLine(int lineForEdit, object newLine)
+        {
+            ArrayList list = ReadLines();
+            list.Insert(lineForEdit, newLine);
+            Write(false, list.ToArray());
+        }
+
+        public void EditLines(int[] linesForEdit, object[] newLines)
+        {
+            if (linesForEdit.Length != newLines.Length)
+            {
+                throw new ArgumentException("The two arrays must have same length.");
+            }
+
+            ArrayList list = ReadLines();
+            for (int i=0; i<linesForEdit.Length; i++)
+            {
+                list.Insert(linesForEdit[i], newLines[i]);
+            }
+            Write(false, list.ToArray());
+        }
+
         public void DeleteLines(params int[] lines)
         {
             ArrayList list = ReadLines();
@@ -45,6 +69,55 @@ namespace DataBaseBackup.Class
             }
 
             Write(false, list.ToArray());
+        }
+
+        /// <summary>
+        /// Opens the stream for reading.
+        /// </summary>
+        /// <param name="step">The amount of lines for read.</param>
+        public void StartReading(int step = 20)
+        {
+            if (index != -1)
+            {
+                throw new Exception("You must first end reading, before call StartReading().");
+            }
+
+            if (step <= 0)
+            {
+                throw new ArgumentException("Argument at pos: 1, can't be <= 0 .");
+            }
+
+            this.step = step;
+            stepReader = new StreamReader(filePath, Encoding.UTF8);
+        }
+
+        /// <summary>
+        /// Read the next lines from the file. The amount of readed lines, is defined at StartReading() method.
+        /// </summary>
+        /// <returns>Return a ListArray with readed lines. If end of stream reached, returns empty ArrayList.</returns>
+        public ArrayList ReadNext()
+        {
+            ArrayList result = new ArrayList();
+            string temp;
+            int i = -1;
+            while ((temp = stepReader.ReadLine()) != null && ++i < step)
+            {
+                result.Add(temp);
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Close the stream. End of reading.
+        /// </summary>
+        public void EndReading()
+        {
+            if (stepReader != null)
+            {
+                stepReader.Close();
+                index = -1;
+            }
         }
 
         public void ClearFile()
