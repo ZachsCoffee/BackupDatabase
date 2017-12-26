9964b7d676bc02f03170a8cd253dea67beb723d5
Zachs <georgenascos96@gmail.com>
2017-12-26 17:36:20 +0200

    Backup update

---
+++ DataBaseBackup/Class/Schedule.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBaseBackup.Class
+{
+    public class Schedule
+    {
+        private DateTime timeStamp, repeatEvery;
+        private string dbName, ftpServerDomainName;
+        private bool repeat = false;
+
+        public Schedule(DateTime timestamp, string dbName, string ftpServerDomainName)
+        {
+            this.timeStamp = timestamp;
+            this.dbName = dbName ?? throw new ArgumentNullException(nameof(dbName));
+            this.ftpServerDomainName = ftpServerDomainName ?? throw new ArgumentNullException(nameof(ftpServerDomainName));
+        }
+
+        public override string ToString()
+        {
+            return "Timestamp: "+timeStamp.ToShortTimeString()+"\t"+timeStamp.ToShortDateString()+"\t" +
+                "Database name: "+dbName+"\tServer name: "+ftpServerDomainName;
+        }
+
+        public void Repeat(DateTime repeatEvery)
+        {
+            if (repeatEvery == null)
+            {
+                throw new ArgumentNullException(nameof(repeatEvery));
+            }
+
+            this.repeatEvery = repeatEvery;
+        }
+
+        public bool IsRepeat
+        {
+            get
+            {
+                return repeat;
+            }
+        }
+    }
+}
