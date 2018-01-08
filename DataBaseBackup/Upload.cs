34fd418f02419b3366c854f4f92748ea78f84755
Neo-Dante <d.dante42327@gmail.com>
2018-01-08 17:54:25 +0200

    update

--- DataBaseBackup/Upload.cs
+++ DataBaseBackup/Upload.cs
@@ -37,27 +37,26 @@ namespace DataBaseBackup
             {
                 if(Form1.testConnectionSFTP(parts[1],parts[2],parts[3],textBox1.Text.ToString()) == Form1.ConnectionStatus.OK)
                 {
-                    label2.Text = "OK";
-                    label2.ForeColor= System.Drawing.Color.Green;
+                    MessageBox.Show("ConnectionStatus: OK");
                     password = textBox1.Text.ToString();
+                    this.Close();
                 }
                 else{
-                    label2.Text = "Failed";
-                    label2.ForeColor = System.Drawing.Color.Red;
+          
+                    MessageBox.Show("ConnectionStatus: Failed");
                 }
             }
             else
             {
                 if(Form1.testConnectionFTP(parts[1], parts[2], parts[3], textBox1.Text.ToString()) == Form1.ConnectionStatus.OK)
                 {
-                    label2.Text = "OK";
-                    label2.ForeColor = System.Drawing.Color.Green;
+                    MessageBox.Show("ConnectionStatus: OK");
                     password = textBox1.Text.ToString();
+                    this.Close();
                 }
                 else
                 {
-                    label2.Text = "Failed";
-                    label2.ForeColor = System.Drawing.Color.Red;
+                    MessageBox.Show("ConnectionStatus: Failed");
                 }
             }
             
